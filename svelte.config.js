20621d767741e61b516ef0e1b8994a65ea99fc79
“Muhammad <al.dighton@outlook.com>
2023-01-27 17:15:11 +0500

    added socketio

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { optimizeImports, optimizeCss, elements } from 'carbon-preprocess-svelte';
 import path from 'node:path';
+
+import { Server } from 'socket.io';
+
+export const webSocketServer = {
+	name: 'webSocketServer',
+	configureServer(server) {
+		const io = new Server(server.httpServer);
+
+		io.on('connect', (socket) => {
+			socket.emit('eventFromServer', 'Hello from the server');
+		});
+	}
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +22,10 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), optimizeImports(), elements()],
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			plugins: [webSocketServer]
+		}
 	},
 	vite: {
 		plugins: [process.env.NODE_ENV === 'production' && optimizeCss()]
