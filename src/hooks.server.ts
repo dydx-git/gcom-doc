9fee6bc6ac79219e470e1e4ad82ac6f6dc0a8837
Muhammad Taha <al.dighton@outlook.com>
2023-02-15 00:30:43 +0500

    added hooks protection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,22 @@
 import { handleHooks } from '@lucia-auth/sveltekit';
 import { auth } from '$lib/models/auth';
 import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle: Handle = handleHooks(auth);
+const handleAuth: Handle = async ({ event, resolve }) => {
+	const pathname = event.url.pathname;
+
+	if (!pathname.startsWith('/login') && !pathname.startsWith('/api/login')) {
+		const { user } = await event.locals.validateUser();
+		if (!user) {
+			return new Response('Unauthorized. Please <a href="login">sign in</a>', {
+				status: 401,
+				headers: {
+					'Content-Type': 'text/html'
+				}
+			});
+		}
+	}
+	return await resolve(event);
+};
+export const handle: Handle = sequence(handleHooks(auth), handleAuth);
