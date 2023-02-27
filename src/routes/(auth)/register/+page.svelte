1f78a1cb54aafb66ddb66c89ef26ed573a319e5e
Muhammad Taha <al.dighton@outlook.com>
2023-02-27 21:55:00 +0500

    Completed registration form

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -1,7 +1,6 @@
-<script>
+<script lang="ts">
 	import ColorButton from '$lib/components/ColorButton.svelte';
 	import ColorGrid from '$lib/components/ColorGrid.svelte';
-	import { HsvPicker } from 'svelte-color-picker';
 	import { CompanyLabel } from '$lib/models/client-form';
 	import {
 		TextInput,
@@ -13,7 +12,9 @@
 		Form,
 		Select,
 		SelectItem,
-		FormLabel
+		FormLabel,
+		ListItem,
+		UnorderedList
 	} from 'carbon-components-svelte';
 
 	let invalid = false;
@@ -22,12 +23,29 @@
 		password: '',
 		company: '',
 		role: '',
-		accentColor: '',
-		secondaryColor: ''
+		colors: [] as string[]
 	};
+	let color: string, centerColor: string, selected: number;
+	let shake: boolean = false;
 
 	$: invalid = !/^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]{6,}$/.test(user.password);
 	let reTypePassword = '';
+
+	const handleColorInput = (e: Event) => {
+		user.colors = [];
+		const target = e.target as HTMLInputElement;
+		centerColor = target.value;
+		selected = 6;
+	};
+
+	const handleSwatchChange = (e: CustomEvent) => {
+		if (user.colors.length >= 5) return;
+
+		color = e.detail.color;
+		selected = e.detail.index;
+		user.colors.push(color);
+		user.colors = [...new Set(user.colors)];
+	};
 </script>
 
 <Grid>
@@ -55,7 +73,7 @@
 							placeholder="Password*"
 							{invalid}
 							required
-							minlength={3}
+							minlength={6}
 							bind:value={user.password}
 						/>
 					</Column>
@@ -67,7 +85,7 @@
 							placeholder="Password*"
 							{invalid}
 							required
-							minlength={3}
+							minlength={6}
 							bind:value={reTypePassword}
 						/>
 					</Column>
@@ -80,26 +98,48 @@
 							{/each}
 						</Select>
 					</Column>
-					<Column md={1} lg={5}>
-						<FormLabel textContent="Pick color">Pick color*</FormLabel>
-						<HsvPicker />
-					</Column>
 					<Row style="margin-left: 0px">
 						<Column>
 							<FormLabel textContent="Pick color">Selected color theme</FormLabel>
-							<div style="height: 50px; width: 50px">
+							<div class="color" style:--color={color} style:--width="96px" style:--height="96px">
+								<input type="color" bind:value={color} on:input={handleColorInput} />
 								<ColorButton />
 							</div>
 						</Column>
 						<Column>
-							<ColorGrid />
+							<ColorGrid {centerColor} {selected} on:colorChange={handleSwatchChange} />
 						</Column>
 					</Row>
+					<Column sm={4} md={3} lg={4}>
+						<FormLabel textContent="Pick color">Selected colors</FormLabel>
+						<UnorderedList expressive>
+							{#each user.colors as color}
+								<ListItem style={`background-color: ${color}`}>{color}</ListItem>
+							{/each}
+						</UnorderedList>
+						<p class:alert={user.colors.length >= 5}>
+							Max 5 colors. Select a color from color button to clear currently selected colors
+						</p>
+					</Column>
 				</Row>
-				<Button type="submit" style="float: right; margin-right: 100px" class="default-gap">
-					Submit
-				</Button>
+				<Button type="submit" style="float: right; margin-right: 100px">Submit</Button>
 			</Form>
 		</Column>
 	</Row>
 </Grid>
+
+<style>
+	input[type='color'] {
+		width: 96px;
+		height: 96px;
+		border: 0;
+		padding: 0;
+		background: transparent;
+		position: absolute;
+		opacity: 0;
+	}
+
+	.alert {
+		color: red;
+	}
+</style>
