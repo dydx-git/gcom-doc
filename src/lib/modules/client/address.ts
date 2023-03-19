6984c75031ff9f04f86fb75006fb9453a000b687
Muhammad Taha <al.dighton@outlook.com>
2023-03-19 14:54:38 +0500

    minor refactoring

---
+++ src/lib/modules/client/address.ts
@@ -0,0 +1,82 @@
+// create class called Address that has the method called getParsedAddress that takes a string and returns Address[]
+
+import { BING_MAPS_ENDPOINT, BING_MAPS_KEY } from "$env/static/private";
+import { error } from "@sveltejs/kit";
+
+export class Address {
+    static async getParsedAddress(address: string): Promise<Address[]> {
+        const response: Response = await fetch(
+            `${BING_MAPS_ENDPOINT}${encodeURIComponent(address)}?includeNeighborhood=0&key=${BING_MAPS_KEY}&strictMatch=1`
+        );
+
+        if (!response.ok) throw error(response.status, { message: response.statusText });
+
+        const result: Address[] = [];
+
+        const parsedResponse: BingAddressResponse = await response.json();
+        const { resourceSets } = parsedResponse;
+        const resourceEntities = resourceSets?.[0];
+
+        if (!resourceEntities || resourceEntities.estimatedTotal === 0 || !resourceEntities.resources)
+            throw error(404, { message: 'No results found' });
+
+        resourceEntities.resources.forEach((resource) => {
+            if (resource.confidence == 'Low') return;
+            const address = resource.address;
+            result.push({
+                formattedAddress: address?.formattedAddress,
+                addressLine: address?.addressLine,
+                city: address?.locality,
+                state: address?.adminDistrict || address?.adminDistrict2,
+                zip: address?.postalCode,
+                country: address?.countryRegion
+            });
+        });
+
+        return result;
+    }
+}
+
+export interface BingAddressResponse {
+    authenticationResultCode: string;
+    brandLogoUri: string;
+    copyright: string;
+    resourceSets?: ResourceSetsEntity[] | null;
+    statusCode: number;
+    statusDescription: string;
+    traceId: string;
+}
+export interface ResourceSetsEntity {
+    estimatedTotal: number;
+    resources?: ResourcesEntity[] | null;
+}
+export interface ResourcesEntity {
+    __type: string;
+    bbox?: number[] | null;
+    name: string;
+    point: Point;
+    address: BingAddress;
+    confidence: string;
+    entityType: string;
+    geocodePoints?: GeocodePointsEntity[] | null;
+    matchCodes?: string[] | null;
+}
+export interface Point {
+    type: string;
+    coordinates?: number[] | null;
+}
+export interface BingAddress {
+    addressLine: string;
+    adminDistrict: string;
+    adminDistrict2: string;
+    countryRegion: string;
+    formattedAddress: string;
+    locality: string;
+    postalCode: string;
+}
+export interface GeocodePointsEntity {
+    type: string;
+    coordinates?: number[] | null;
+    calculationMethod: string;
+    usageTypes?: string[] | null;
+}
--- src/routes/(api)/api/address/+server.ts
+++ src/routes/(api)/api/address/+server.ts
@@ -1,84 +1,12 @@
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { BING_MAPS_ENDPOINT, BING_MAPS_KEY } from '$env/static/private';
-import type { Address } from '$lib/modules/common/interfaces/form';
+import { Address } from '$lib/modules/client/address';
 
 export const GET: RequestHandler = async (req) => {
 	const inputAddress = req.url.searchParams.get('address');
 	if (!inputAddress) throw error(400, { message: 'Missing parameter: address' });
 
-	const address = encodeURIComponent(inputAddress);
-	const response: Response = await fetch(
-		`${BING_MAPS_ENDPOINT}${address}?includeNeighborhood=0&key=${BING_MAPS_KEY}&strictMatch=1`
-	);
-
-	if (!response.ok) throw error(response.status, { message: response.statusText });
-
-	const result: Address[] = [];
-
-	const parsedResponse: BingAddressResponse = await response.json();
-	const { resourceSets } = parsedResponse;
-	const resourceEntities = resourceSets?.[0];
-
-	if (!resourceEntities || resourceEntities.estimatedTotal === 0 || !resourceEntities.resources)
-		throw error(404, { message: 'No results found' });
-
-	resourceEntities.resources.forEach((resource) => {
-		if (resource.confidence == 'Low') return;
-		const address = resource.address;
-		result.push({
-			formattedAddress: address?.formattedAddress,
-			addressLine: address?.addressLine,
-			city: address?.locality,
-			state: address?.adminDistrict || address?.adminDistrict2,
-			zip: address?.postalCode,
-			country: address?.countryRegion
-		});
-	});
+	const result = Address.getParsedAddress(inputAddress);
 
 	return json(result);
 };
-
-export interface BingAddressResponse {
-	authenticationResultCode: string;
-	brandLogoUri: string;
-	copyright: string;
-	resourceSets?: ResourceSetsEntity[] | null;
-	statusCode: number;
-	statusDescription: string;
-	traceId: string;
-}
-export interface ResourceSetsEntity {
-	estimatedTotal: number;
-	resources?: ResourcesEntity[] | null;
-}
-export interface ResourcesEntity {
-	__type: string;
-	bbox?: number[] | null;
-	name: string;
-	point: Point;
-	address: BingAddress;
-	confidence: string;
-	entityType: string;
-	geocodePoints?: GeocodePointsEntity[] | null;
-	matchCodes?: string[] | null;
-}
-export interface Point {
-	type: string;
-	coordinates?: number[] | null;
-}
-export interface BingAddress {
-	addressLine: string;
-	adminDistrict: string;
-	adminDistrict2: string;
-	countryRegion: string;
-	formattedAddress: string;
-	locality: string;
-	postalCode: string;
-}
-export interface GeocodePointsEntity {
-	type: string;
-	coordinates?: number[] | null;
-	calculationMethod: string;
-	usageTypes?: string[] | null;
-}
