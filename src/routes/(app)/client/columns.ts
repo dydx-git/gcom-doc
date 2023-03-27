ab485abbf304a57861293f88f09dec13bd03969a
Muhammad Taha <al.dighton@outlook.com>
2023-03-27 12:06:48 -0700

    fixed client table layout column widths a bit and other minor bugfixes

--- src/routes/(app)/client/columns.ts
+++ src/routes/(app)/client/columns.ts
@@ -6,17 +6,17 @@ export const clientDatatableColumnKeys = {
     salesRep: 'salesRep',
     email: 'emails',
     phone: 'phones',
-    address: 'address',
+    address: 'clientAddress',
     payMethod: 'payMethod',
     actions: 'actions'
 };
 
 export const clientColumns: ReadonlyArray<DataTableHeader> = [
     { key: clientDatatableColumnKeys.name, value: 'Name' },
-    { key: clientDatatableColumnKeys.salesRep, value: 'Sales Rep' },
-    { key: clientDatatableColumnKeys.email, value: 'Email', display: (emails) => emails.map((email: ClientEmailOptionalDefaults) => email.email).join('/') },
+    { key: clientDatatableColumnKeys.salesRep, value: 'Sales Rep', display: (salesRep) => salesRep.name },
+    { key: clientDatatableColumnKeys.email, value: 'Email', display: (emails) => emails.map((email: ClientEmailOptionalDefaults) => email.email).join('/'), width: '16rem' },
     { key: clientDatatableColumnKeys.phone, value: 'Phone', display: (phones) => phones.map((phone: ClientPhoneOptionalDefaults) => phone.phone).join('/') },
-    { key: clientDatatableColumnKeys.address, value: 'Address', display: (address) => `${address?.street}, ${address?.city}, ${address?.state} ${address?.zip}` },
+    { key: clientDatatableColumnKeys.address, value: 'Address', display: (address) => `${address.address}, ${address.city}, ${address.state} ${address.zip}`, width: '20rem' },
     { key: clientDatatableColumnKeys.payMethod, value: 'Pay Method' },
     { key: clientDatatableColumnKeys.actions, value: 'Actions', sort: false, width: '5rem' }
 ]
--- src/routes/(auth)/+error.svelte
+++ src/routes/(auth)/+error.svelte
@@ -1,11 +1,1 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-
-	const error = $page.error as App.Error;
-</script>
-
-<h2>{error.code}: {error?.message ?? error?.messages}</h2>
-
-{#if error.code == 401}
-	Please go back to the <a href="/">homepage</a>
-{/if}
+Please go back to the <a href="/">homepage</a>
